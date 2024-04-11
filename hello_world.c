40f40e3e85956ed243c92af5735836b1a9d38d4b
lokeshkaushik07 <lokeshjnv2004@gmail.com>
2024-04-11 23:14:35 +0530

    diff types of data types

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-    printf("1\t2\t3\n4\t\"5\"\t6\n7\t8\t9\xhh__");
+    printf("1\t2\t3\n4\t\"5\"\t6\n7\t8\t9\n");
     printf("I like pizza - lokesh");
     return 0;
 }

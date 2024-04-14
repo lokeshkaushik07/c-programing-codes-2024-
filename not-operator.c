a8a7ef91b6a9e622aa059bcb791110fd61ef95bd
lokeshkaushik07 <lokeshjnv2004@gmail.com>
2024-04-14 15:49:54 +0530

    not operator simply reverse the state of condition

---
+++ not-operator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    // !not operator reverses the state of a condition 
+  
+  bool sunny = false;
+
+  if(sunny){
+    printf("\nIt's cloudy outside!");
+  }
+  else{
+    printf("\nIts sunny outside!");
+  }
+    return 0;
+}  
